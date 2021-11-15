a5b33473386c10f762e65a061be102b1bdbb9c53
boris <bolik-u13@mail.ru>
2021-11-15 10:55:00 +0300

    [server]: add folder skeleton

--- server/src/testlib.h
+++ server/src/testlib.h
@@ -5,11 +5,16 @@
 #ifndef GTEST_DEMO_TESTLIB_H
 #define GTEST_DEMO_TESTLIB_H
 
+//#include "logger.h"
+
 class TestLib {
 public:
   double add_numbers(const double f1, const double f2);
   double subtract_numbers(const double f1, const double f2);
   double multiply_numbers(const double f1, const double f2);
+
+private:
+//    Logger logger;
 };
 
 #endif // GTEST_DEMO_TESTLIB_H
